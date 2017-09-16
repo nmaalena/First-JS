1979c4eaee139928bc8019e2efc14359d0a044e4
unknown <nmaalena@uwaterloo.ca>
2017-09-16 17:01:59 -0400

    test1021

--- platformer.js
+++ platformer.js
@@ -133,6 +133,7 @@
       document.getElementById("barbarybread").innerHTML = "<center><font color='white'><h2>Lives remaining: " + --life + "</h2></font></center>";
     else
       document.getElementById("barbarybread").innerHTML = "<center><font color='white'><h2>YOU HAVE DIED</h2></font></center>";
+      COLOR    = { BLACK: '#000000', YELLOW: BLACK, BRICK: BLACK, PINK: BLACK, PURPLE: BLACK, GREY: BLACK, SLATE: BLACK, GOLD: BLACK},
   }
 
   function collectTreasure(t) {
